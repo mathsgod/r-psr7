9e4c5ee9dd68451d9ef66b1e6ecc6b3a718d5cc7
Raymond Chong <mathsgod@yahoo.com>
2022-04-06 17:17:02 +0800

    update 8.0

--- demo/index.php
+++ demo/index.php
@@ -1,4 +1,4 @@
-<?
+<?php
 
 use R\Psr7\Stream;
 
--- tests/ServerRequestTest.php
+++ tests/ServerRequestTest.php
@@ -1,4 +1,5 @@
-<?
+<?php
+
 
 declare(strict_types=1);
 error_reporting(E_ALL & ~E_WARNING & ~E_NOTICE);
@@ -24,7 +25,7 @@ final class ServerRequestTest extends TestCase
 
     public function test_create()
     {
-        $r = new ServerRequest("GET", Uri::CreateFromString("https://raymond.hostlink.com.hk/test"));
+        $r = new ServerRequest();
         $this->assertInstanceOf(ServerRequest::class, $r);
     }
 }
